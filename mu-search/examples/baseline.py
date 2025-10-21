9cdca686cd6b68beb41c697f8d2da514f6b8145d
zhiyu-zhao-ucas <zhaozhiyu22@mails.ucas.ac.cn>
2025-10-21 21:29:09 +0800

    fix algo name

--- mu-search/examples/baseline.py
+++ mu-search/examples/baseline.py
@@ -182,7 +182,7 @@ def main(args):
                 rounds=10,
                 log_file=f'efficiency/{args.method}/{args.landscape}/{sequences_batch_size}_{model_queries_per_batch}_{args.run}.csv',
             )
-        elif 'musearch' in args.method:
+        elif args.method == 'musearch':
             musearch_args = argparse.Namespace(
                 score_threshold=-np.inf,
                 horizon=5,
